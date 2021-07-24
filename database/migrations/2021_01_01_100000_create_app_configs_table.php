df472b2b619d145a6d5b4d165939733cc06284de
Raju Koyilandy <mkrajesh.kdy@gmail.com>
2021-07-24 16:51:54 +0530

    Initial commit

---
+++ database/migrations/2021_01_01_100000_create_app_configs_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAppConfigsTable extends Migration
+{
+    /**
+     * The database schema.
+     *
+     * @var \Illuminate\Database\Schema\Builder
+     */
+    protected $schema;
+
+    /**
+     * Create a new migration instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->schema = Schema::connection($this->getConnection());
+    }
+
+    /**
+     * Get the migration connection name.
+     *
+     * @return string|null
+     */
+    public function getConnection()
+    {
+        return config('vocolab.storage.database.connection');
+    }
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
---
+++ src/VocoLabConfigServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace VocoLab\AppConfig;
+
+use Illuminate\Support\ServiceProvider;
+
+class VocoLabConfigServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any package services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
